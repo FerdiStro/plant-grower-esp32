4f96a8269c68dd5b3e56e6851369eadef6bd0430
ferdinands <ferdinand.strobel@infrontfinance.com>
2023-05-19 13:01:49 +0200

    Fully work with mqtt,but maybe swtich to http

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,24 @@
 #include <esp_adc_cal.h>
 #include <driver/adc.h>
+#include <WiFi.h>
+#include <AsyncMqttClient.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
 
-const byte debugLed = 13; //status Light on port PIN13
+#define WIFI_SSID "LamasParty box"
+#define WIFI_PASSWORD "99808417320259711798"
+#define MQTT_PUB_MOS "esp32/mos/andi"
+
+#define MQTT_HOST IPAddress(192, 168, 178, 152)
+#define MQTT_PORT 1883
+
+//set to true if Credentials needed
+const boolean needCreed = false;
+#define MQTT_USER ""
+#define MQTT_PW ""
+
 const byte dataIn = 21;
 
 static const float WET_MAX = 55.00;
@@ -10,24 +27,94 @@ static const float OK_MIN = 75.10;
 static const float OK_MAX = 83.00;
 static const float DRY_MIN = 83.10; 
 
-void setup(){
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+unsigned long previousMillis = 0;
+//Interval
+const long interval = 10000;
+
+
+void setup(){  
   Serial.begin(115200); 
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+  WiFi.onEvent(WiFiEvent);
+  WiFi.setHostname(String(MQTT_PUB_MOS).c_str());
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   adc1_config_width(ADC_WIDTH_BIT_12);
   adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_0);
-  pinMode(debugLed, OUTPUT); 
   pinMode(dataIn, INPUT);
+  if(needCreed){
+    mqttClient.setCredentials(MQTT_USER, MQTT_PW);
+  }
+
+  connectToWifi();
  
 } 
 
 void loop() {
-   digitalWrite(debugLed, HIGH);
-   int sensorValue = analogRead(32);
-   float moisture = map(sensorValue, 0, 4095, 0, 100);
-   Serial.print("Moisture level: ");
-   Serial.println(moisture);
-   delay(1000);
 
+   unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval) {
+      
+      previousMillis = currentMillis;
+      int sensorValue = analogRead(32);
+      float moisture = map(sensorValue, 0, 4095, 0, 100);
+      
+      if(isnan(moisture)){
+        Serial.println(F("FAILED to read sensor!"));
+        return;
+      }
 
- 
+      uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_MOS, 1, true, String(moisture).c_str());    
+      Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_MOS, packetIdPub1);
+      Serial.printf("Message: %.2f \n", moisture);
+   
+   }
+}
 
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  delay(10000);
+}
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0);
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
 }
